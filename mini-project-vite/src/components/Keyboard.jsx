91dc38606eeb53d27f72b7f250cdefed1e8567a6
orli-melkam <orlimelkam@hilma.tech>
2024-10-31 12:35:47 +0200

    style

--- mini-project-vite/src/components/Keyboard.jsx
+++ mini-project-vite/src/components/Keyboard.jsx
@@ -1,14 +1,16 @@
 import React, {useState} from "react";
 import ChooseLanguage from "./languages";
 
-const English = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm']
-const Hebrew = ['ק', 'ר', 'א', 'ט', 'ו', 'ן', 'ם', 'פ', 'ש', 'ד', 'ג', 'כ', 'ע', 'י', 'ח', 'ח', 'ל', 'ך', 'ף', 'ז', 'ס', 'ב', 'ה', 'נ', 'מ', 'צ', 'ת']
-const colors = ["red", "orange", "yellow", "green", "blue", "purple", "black"]
+const English = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
+const Hebrew = ['ק', 'ר', 'א', 'ט', 'ו', 'ן', 'ם', 'פ', 'ש', 'ד', 'ג', 'כ', 'ע', 'י', 'ח', 'ח', 'ל', 'ך', 'ף', 'ז', 'ס', 'ב', 'ה', 'נ', 'מ', 'צ', 'ת'];
+const colors = ["red", "orange", "yellow", "green", "blue", "purple", "black"];
 const sizes = [10, 20, 30, 40, 50];
+const fontFamilies = ['sans-serif', 'serif', 'monospace'];
 
 let myStyle = {
     color: "black",
-    fontSize: 20
+    fontSize: 20,
+    fontFamily: 'sans-serif'
 }
 
 function Keybord(){
@@ -49,16 +51,31 @@ function Keybord(){
         })
     }
     function changeTextSize(event){
-        console.log({...myStyle, fontSize: event.target.value})
         setColorStyle((prev) => {
-            return {...prev, fontSize: event.target.value}
+            console.log({...prev, fontSize: parseInt(event.target.value)})
+            return {...prev, fontSize: parseInt(event.target.value)}
         });
     }
-
+    function changeFontFamily(event){
+        setColorStyle((prev) => {
+            console.log({...prev, fontFamily: event.target.value})
+            return {...prev, fontFamily: event.target.value}
+        });
+    }
     function changeInputValue() {
         for(let i = 0; i < inputValue.length; i++) {
             return inputValue[i]
         }
+    }
+    function deleteLastChar(){
+        setInputValue((prev)=>{
+            let temp = [...prev]
+            temp.pop();
+            return temp
+    });
+    }
+    function resetText(){
+        setInputValue([]);
     }
 
     console.log('inputValue: ', inputValue);
@@ -66,11 +83,16 @@ function Keybord(){
         <>
             <div onChange={changeInputValue} style={style}>
                 {inputValue.map((item, index) => {
-                    console.log(item.value);
+                    console.log(item.style);
                     return <span key={index} style={item.style} value={item.value}>{item.value}</span>
                 })}
             </div>
-                <br /><br />
+            <br /><br />
+            <div>
+                <button onClick={deleteLastChar}>delete</button>
+                <button onClick={resetText}>reset</button>
+            </div>
+                
             <div className="keyboard">
                 {language === 'English'? English.map((item, index) => (
                     <button key={index} onClick={addChar}>{item}</button>
@@ -97,6 +119,13 @@ function Keybord(){
                 <select onChange={changeTextSize}>
                     {sizes.map((size, index) => (
                         <option key={index} value={size}>{size}</option>
+                    ))}
+                </select>
+            </div>
+            <div>
+                <select onChange={changeFontFamily}>
+                    {fontFamilies.map((font, index) => (
+                        <option key={index} value={font}>{font}</option>
                     ))}
                 </select>
             </div>
